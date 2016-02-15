53afaa2c842299433f8847fff2e919b46969d35d
zsiswick <zsiswick@cisco.com>
2016-02-15 00:07:05 -0500

    fix localhost issue in dev environment

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,17 @@
 $(document).foundation();
 $(document).ready(function() {
+
+    var baseurl;
+    var environ = window.location.host;
     
-    var baseurl = window.location.protocol + "//" + window.location.host + "/";
+    if(environ.indexOf('localhost') > -1) {
+      baseurl = window.location.protocol + "//" + window.location.host + "/" + "rubyinvoice/";
+    } else {
+      baseurl = window.location.protocol + "//" + window.location.host + "/";
+    }
     var id = window.location.pathname.split('/').pop();
     var request;
-    
+
     // PREVENT INVALID CHARACTERS ENTERED INTO TXT FIELDS
         $( "#invoiceCreate, #paymentModal" ).on( "keypress", "input.qty, input.sum, input.amt", function(e) {
           var chr = String.fromCharCode(e.which);
@@ -12,31 +19,31 @@ $(document).ready(function() {
           	return false;
           }
         });
-    
+
         $('#nav-container-mobile').click(function() {
         	$('#main-menu-mobile').toggle(function() {
         		$('#main-menu-mobile-toggle').toggleClass('open');
         	});
         });
-    
+
         // DO THE HARLEM SHAKE
     		$('form').on('invalid.fndtn.abide', function() {
           var jThis=$(this);
-    
+
           	jThis.addClass('shake');
           	jThis.bind('webkitAnimationEnd mozAnimationEnd oAnimationEnd msAnimationEnd animationEnd',function(e){
           		jThis.removeClass('shake');
           	});
           	return false;
-    
+
         });
-    
+
         function init_autonumeric() {
         	if ( $("#invoiceCreate").length == 1 ) {
         		$('.sum, .totalSum, #invoiceSubtotal, #taxOne, #taxTwo, #discount').autoNumeric('init', {aDec:'.', aSep:'', aForm: false});
         	}
         }
-    
+
         function calculateTotal() {
         	var tax1 = $("#taxOne").autoNumeric('get');
         	var tax2 = $("#taxTwo").autoNumeric('get');
@@ -44,7 +51,7 @@ $(document).ready(function() {
         	var discount = $("#discount").autoNumeric('get');
         	var totalSum;
         	totalAmnt = (+tax1 + +tax2) + +subtotal;
-    
+
         	if (discount > totalAmnt)
         	{
         		totalSum = 0;
@@ -53,10 +60,10 @@ $(document).ready(function() {
         	{
         		totalSum = (+totalAmnt - +discount);
         	}
-    
+
         	$("#invoiceTotal").autoNumeric('set', totalSum);
         }
-    
+
         function calculateTaxTotal() {
           init_autonumeric();
         	var	tax1 = 0;
@@ -67,9 +74,9 @@ $(document).ready(function() {
         	var percent2 = 0;
         	var sum1 = 0;
         	var sum2 = 0;
-    
+
           if ( $(".tax").length ) {
-    
+
             $(".tax option:selected").each(function()
             {
               if ( $(this).val() == $("#invoice_tax_1").val() )
@@ -93,14 +100,14 @@ $(document).ready(function() {
               $("#taxOne").autoNumeric('set', sum1);
               $("#taxTwo").autoNumeric('set', sum2);
             });
-    
+
           } else {
             $("#taxOne").autoNumeric('set', 0);
             $("#taxTwo").autoNumeric('set', 0);
           }
           calculateTotal();
         }
-    
+
         function calculateSubTotal() {
             var sum = 0;
             var amnt = 0;
@@ -110,53 +117,53 @@ $(document).ready(function() {
               amt = parseFloat( $(this).autoNumeric('get') );
               sum += amt;
             });
-    
+
             //.toFixed() method will roundoff the final sum to 2 decimal places
             $("#invoiceSubtotal").autoNumeric('set', sum ).effect("highlight", {}, 1250);
             calculateTaxTotal();
         }
-    
+
         // CALCULATE THE ITEM ROWS ON EDIT INVOICE PAGE
        	$(document).on('focusout', ".sum", function(){
-    
+
         	init_autonumeric();
         	var $this = $(this);
         	var qty = $this.parent().parent().parent().parent().find('input.qty').val();
-    
+
         	var num = $this.parent().parent().parent().parent().find('.unitCost').autoNumeric('get');
         	var sumtotal = parseFloat((num * qty));
-    
+
         	$this.parent().parent().parent().parent().find('.totalSum').autoNumeric('set', sumtotal );
         	calculateSubTotal();
-    
+
         });
-    
+
         $(document).on('change', ".tax, #invoice_tax_1, #invoice_tax_2, #invoiceDiscount", function(){
         	calculateTaxTotal();
         });
-    
+
         $(document).on('click', ".delete-row", function() { // RECALCULATE WHEN ITEMS ARE REMOVED
         	$(this).parent().parent().parent().parent().parent().remove();
         	$val = $(this).parent().parent().parent().prev().children().children('input'[name="item_id[]"]).val();
         	// CREATE HIDDEN INPUT LIST OF RECORDS TO BE DELETED
         	$('.edit-list-container div.tabbed.list').append('<input type="hidden" name="delete_ids[]" value="'+$val+'"></input>');
-    
+
         	calculateSubTotal();
         });
-    
+
         if ( $("#invoiceCreate").length ) {
         	calculateTaxTotal();
         	calculateSubTotal();
         }
-    
+
         function add_client_callback(cl_data) {
-    
+
         	setTimeout(function() {
         	      location.reload();
         	}, 1000);
-    
+
         }
-    
+
         // AJAX FORMS
         function ajaxRequest($this, url, callback) {
         	// abort any pending request
@@ -167,14 +174,14 @@ $(document).ready(function() {
           var $inputs = $form.find("input, select, button, textarea");
           // serialize the data in the form
           var serializedData = $form.serialize();
-    
+
           // let's disable the inputs for the duration of the ajax request
           // Note: we disable elements AFTER the form data has been serialized.
           // Disabled form elements will not be serialized.
           $inputs.prop("disabled", true);
-    
+
           $('#loadingImg').show();
-    
+
           // fire off the request
           request = $.ajax({
               url: baseurl+url,
@@ -183,21 +190,21 @@ $(document).ready(function() {
               dataType: 'json',
               cache:false,
               success: function(respond) {
-    
+
                 if (respond.result == 'false') {
-    
+
                 	var keys = Object.keys(respond);
-    
+
                 	$('#form-errors').html(respond.errors).addClass("").show();
-    
+
                 } else {
-    
+
                  $('#form-errors').html(respond.errors).addClass("").show();
-    
+
                  if(typeof respond.redirect != "undefined") window.location.href = respond.redirect;
-    
+
                  if(typeof respond.records != "undefined") callback(respond.records);
-    
+
                 }
               }
           });
@@ -211,21 +218,21 @@ $(document).ready(function() {
           // prevent default posting of form
           event.preventDefault();
         }
-    
+
         $( "#form-wrap" ).on( "submit", "#addClient", function(event) {
           var id = window.location.pathname.split('/').pop();
           $this = $(this);
           ajaxRequest($this, 'index.php/clients/create', add_client_callback);
         });
-    
+
         // GET VARIOUS MODAL WINDOW CONTENT
-    
+
         // GENERIC CONTENT LOADER FUNCTION
         function load_content(url) {
         	$("#form-wrap").hide();
         	$('#loadingImg').show();
         	var id = window.location.pathname.split('/').pop();
-    
+
         	$.get( url, function( data ) {
         	  $("#form-wrap").html( data );
         	  $("#form-errors").hide();
@@ -234,49 +241,49 @@ $(document).ready(function() {
             $('#form-wrap').show();
         	});
         }
-    
+
         $("#addPaymentBtn").on("click", function() {
         	load_content(baseurl+"index.php/invoices/view_payments/"+id);
         });
-    
+
         $('[name="client"]').on("change", function() {
         	if ( $(this).val() == "add_new_client") {
         		$('#revealModal').foundation('reveal', 'open');
         		load_content(baseurl+"index.php/clients/create_ajax");
         	}
         });
-    
+
         $("#sendInvoiceBtn").on("click", function() {
         	load_content(baseurl+"index.php/invoices/view_invoice_email/"+id+"/0");
         });
-    
+
         $("#sendInvoiceRemindBtn").on("click", function() {
         	load_content(baseurl+"index.php/invoices/view_invoice_email/"+id+"/1");
         });
-    
+
         $("#sendInvoiceThanks").on("click", function() {
         	load_content(baseurl+"index.php/invoices/view_invoice_email/"+id+"/2");
         });
-    
+
         // END
-    
+
         $("#auto_reminder").on("click", function() {
         	var id = window.location.pathname.split('/').pop();
         	var checked = 0;
         	$this = $(this);
-    
+
         	$("#form-wrap").hide();
         	$('#loadingImg').show();
-    
+
         	if ($('#auto_reminder').is(':checked')) {
         		checked = 1;
         	}
-    
+
         	$.ajax({
         	    type: 'POST',
         	    url: baseurl+"index.php/invoices/set_auto_reminder/"+id+"/"+checked,
         	    success: function(msg) {
-    
+
         	    	$('#loadingImg').hide();
         	    	$('#form-wrap').show();
         	    	$('#paymentModal').foundation('reveal', 'open');
@@ -285,13 +292,13 @@ $(document).ready(function() {
         	    		} else {
         	    			$("#form-wrap").html( '<div class="row"><div class="small-12 columns text-center"><h2>Auto Reminder</h2><p>Auto-Reminder is off</p></div></div><a class="close-reveal-modal">&#215;</a>' );
         	    		}
-    
+
         	    }
         	  });
         });
-    
+
         // ADD FAVORITE INVOICE ITEM
-    
+
         $( ".edit-list-container" ).on( "click", ".add-favorite", function(event) {
           var postData = {
             "qty" : $(this).closest('div.qty').find('input.qty').val(),
@@ -299,7 +306,7 @@ $(document).ready(function() {
             "description" : $(this).closest('div.tabbed').find('textarea').val(),
             "unit_cost" : $(this).closest('div.tabbed').find('input.unitCost').val()
           };
-    
+
           $.ajax({
               type: "POST",
               url: baseurl+"index.php/invoices/add_favorite_invoice_item",
@@ -310,24 +317,24 @@ $(document).ready(function() {
               }
           });
         });
-    
+
         $("#cancelDeleteBtn").on("click", function() {
         	$('#editModal').foundation('reveal', 'close');
         });
-    
+
         $("#enable_payments").on("click", function() {
-    
+
         		if ( $('#enable_payments:checked').length >0 ) {
         			$("#payment_settings").slideDown("fast");
         		} else {
         			$("#payment_settings").slideUp("fast");
         		}
         });
-    
+
         if ( $('#enable_payments:checked').length >0 ) {
         	$("#payment_settings").show();
         } else {
         	$("#payment_settings").hide();
         }
-       
+
 });
